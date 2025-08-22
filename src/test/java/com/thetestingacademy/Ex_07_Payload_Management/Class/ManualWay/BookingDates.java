019a0f20e65c12c5d72e64e661fd743040b6288a
Viku786 <kkvikash2016@gmail.com>
2025-08-22 13:04:56 +0530

    POJO Push

---
+++ src/test/java/com/thetestingacademy/Ex_07_Payload_Management/Class/ManualWay/BookingDates.java
@@ -0,0 +1,24 @@
+package com.thetestingacademy.Ex_07_Payload_Management.Class.ManualWay;
+
+public class BookingDates {
+
+    private String checkin;
+    private String checkout;
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+
+}
---
+++ src/test/java/com/thetestingacademy/Ex_07_Payload_Management/Class/Tools/Response_POJO/BookingResponse.java
@@ -0,0 +1,36 @@
+package com.thetestingacademy.Ex_07_Payload_Management.Class.Tools.Response_POJO;
+
+import com.thetestingacademy.Ex_07_Payload_Management.Class.Tools.Request_POJO.Booking;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class BookingResponse {
+
+    private Integer bookingid;
+    private Booking booking;
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    public Integer getBookingid() {
+        return bookingid;
+    }
+
+    public void setBookingid(Integer bookingid) {
+        this.bookingid = bookingid;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+}
